0cedb0d48cc22e4b98a1c83aac7c82c1c96fa3c4
acdevs <amanchandra.in@gmail.com>
2023-05-27 05:06:37 +0530

    updates.

--- scripts/main-app.js
+++ scripts/main-app.js
@@ -1,18 +1,3 @@
-
-function marquee(element) {
-    let elementWidth = element.offsetWidth;
-    let parentWidth = element.parentElement.offsetWidth;
-    let flag = 0;
-  
-    setInterval(() => {
-        element.style.marginLeft = --flag + "px";
-  
-        if (elementWidth == -flag) {
-            flag = parentWidth;
-        }
-    }, 10);
-}
-
 const sliderPrevbtn = document.querySelector(".slider-prev");
 const sliderNextbtn = document.querySelector(".slider-next");
 const sliderViewContext = document.querySelector(".view-event-cards");
