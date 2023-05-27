bc65a005bdd75cd403e2c3621a7bfd7a357584ec
acdevs <amanchandra.in@gmail.com>
2023-05-27 15:51:28 +0530

    res. updates.

--- scripts/main-app.js
+++ scripts/main-app.js
@@ -1,9 +1,21 @@
 const sliderPrevbtn = document.querySelector(".slider-prev");
 const sliderNextbtn = document.querySelector(".slider-next");
 const sliderViewContext = document.querySelector(".view-event-cards");
+const navBarItems = document.querySelector(".nav-bar-items");
+
 sliderNextbtn.addEventListener("click", () => {
     sliderViewContext.scrollLeft += 2000;
 });
 sliderPrevbtn.addEventListener("click", () => {
     sliderViewContext.scrollLeft -= 2000;
+});
+
+document.querySelector(".nav-bar-hamburger i").addEventListener("click", (e) => {
+    navBarItems.classList.toggle("nav-bar-items-active");
+    e.target.classList.toggle("nav-bar-hamburger-active");
+});
+document.querySelectorAll(".item-link a").forEach((item) => {
+    item.addEventListener("click", () => {
+        navBarItems.classList.toggle("nav-bar-items-active");
+    })
 });
