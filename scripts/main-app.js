dbdd94778318e6cb54490a4eb5843102db39cc62
acdevs <amanchandra.in@gmail.com>
2023-06-06 20:09:54 +0530

    plantation drive event update.

--- scripts/main-app.js
+++ scripts/main-app.js
@@ -21,3 +21,42 @@ document.querySelectorAll(".item-link a").forEach((item) => {
         navBarHam.classList.toggle("nav-bar-hamburger-active");
     })
 });
+
+function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+  }
+  
+const photos = document.getElementsByClassName("photo");
+
+for (let i = photos.length - 1; i >= 0; i--) {
+    const relZInd = i + 100;
+    photos[i].style.zIndex = relZInd;
+
+    const rotation = getRandomArbitrary(6, -6);
+    const xMargin = getRandomArbitrary(45, -45);
+    const yMargin = getRandomArbitrary(45, -45);
+    photos[i].style.webkitTransform = 'rotate(' + rotation + 'deg) translateY(' + yMargin + 'px) translateX(' + xMargin + 'px)';
+    photos[i].style.MozTransform = 'rotate(' + rotation + 'deg)';
+    photos[i].style.msTransform = 'rotate(' + rotation + 'deg)';
+    photos[i].style.transform = 'rotate(' + rotation + 'deg)';
+}
+  
+let count = photos.length - 1;
+
+function photoClickHandler() {
+    let topElement = photos[count];
+    let currentIndex = parseInt(getComputedStyle(topElement).zIndex);
+    let newIndex = currentIndex - photos.length;
+    topElement.style.zIndex = newIndex;
+    count--;
+
+    if (count === -1) {
+        count = photos.length - 1;
+    }
+}
+  
+  const photoElements = document.getElementsByClassName("photo");
+  for (let i = 0; i < photoElements.length; i++) {
+    photoElements[i].addEventListener('click', photoClickHandler);
+  }
+  
